27821dcbc9a3b560b7e8e997d4396fc3337cf6a3
m95416848-dev <m95416848@gmail.com>
2026-04-04 20:00:32 +0300

    Update uni_mouse_quadrature.c

--- src/components/bluepad32/uni_mouse_quadrature.c
+++ src/components/bluepad32/uni_mouse_quadrature.c
@@ -40,7 +40,7 @@
 #define TASK_TIMER_PRIO (10)
 
 // Default scale factor for the mouse movement
-#define DETAULT_SCALE_FACTOR (10)
+#define DETAULT_SCALE_FACTOR (5)
 
 enum direction {
     PHASE_DIRECTION_NEG,
